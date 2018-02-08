6245544010de9d87fb5c66f8ef6a5f8a535e6763
Stoyan <slownessbg@gmail.com>
2018-02-08 21:07:52 +0200

    Adding ThirdDigit program

---
+++ ConsoleApp1/ThirdDigit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+            if (number.ToString().Length < 3)
+            {
+                Console.WriteLine("False 0");
+            }
+            else
+            {
+                string numberStr = number.ToString();
+                string filter = numberStr.Substring(numberStr.Length - 3);
+                char filter2 = filter[filter.Length - 3];
+                int result = (int) Char.GetNumericValue(filter2);
+                if (result == 7)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False {0}", result);
+                }
+            }
+        }
+    }
+}
