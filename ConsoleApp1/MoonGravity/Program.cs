4d392a398a6f5bfac4908b13c20d05eec0131c39
Stoyan <slownessbg@gmail.com>
2018-02-08 20:01:39 +0200

    Fixing some other stuff v2.0

---
+++ ConsoleApp1/MoonGravity/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoonGravity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //The gravitational field of the Moon is approximately 17% of that on the Earth.
+            //Write a program that calculates the weight of a man on the moon by a given weight W(as a floating-point number) on the Earth.
+            //The weight W should be read from the console.
+
+            //Input
+            //The input will consist of a single line containing a single floating-point number - the weight W.
+            //Output
+            //Output a single floating-point value - how much a man with the weight W on Earth will weigh on the Moon. Output all 
+            //values with exactly 3-digit precision after the floating point.
+
+            double number = double.Parse(Console.ReadLine());
+            double result = number / 100 * 17;
+            Console.WriteLine(Math.Round(result,3));
+        }
+    }
+}
---
+++ ConsoleApp1/TrainingBitsOperation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingBitsOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 4;
+
+            int result = (num << 1);
+            if (result == 8)
+            {
+                Console.WriteLine(result);
+            }
+
+            int result1 = (num >> 1);
+            if (result1 == 2)
+            {
+                Console.WriteLine(result1);
+            }
+
+            int pos = 5;
+            int number = 35;
+            int mask = 32;
+            int numberAndMask = number & mask;
+
+            if (numberAndMask > 0)
+            {
+                Console.WriteLine("For {0} bit at position {1} is 1", number, pos);
+            }
+            else
+            {
+                Console.WriteLine("For {0} bit at position {1} is 0", number, pos);
+            }
+           
+
+        }
+    }
+}
