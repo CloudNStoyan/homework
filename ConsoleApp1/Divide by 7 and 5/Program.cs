89fa3ca7dff345f765d8fae1b41d9dd6a5623547
Stoyan <slownessbg@gmail.com>
2018-02-08 20:37:54 +0200

    Adding Rectangles program

--- ConsoleApp1/Divide by 7 and 5/Program.cs
+++ ConsoleApp1/Divide by 7 and 5/Program.cs
@@ -11,6 +11,18 @@ namespace Divide_by_7_and_5
     {
         static void Main(string[] args)
         {
+            //Write a program that does the following:
+            //Reads an integer number from the console.
+            //Stores in a variable if the number can be divided by 7 and 5 without remainder.
+            //Prints on the console "true NUMBER" if the number is divisible without remainder by 7 and 5. Otherwise prints "false 
+            //NUMBER". In place of NUMBER print the value of the input number.
+
+            //Input
+            //The input will consist of a single integer value.
+            //Output
+            //The output must always follow the format specified in the description. See the sample tests.
+
+
             int input = int.Parse(Console.ReadLine());
 
             if (input % 7 == 0 && input % 5 == 0)
---
+++ ConsoleApp1/Rectangles/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write an expression that calculates rectangle’s area and perimeter by given width and height. The width and height should be read from the console.
+            //Input
+            //The input will consist of 2 lines:
+            //On the first line, you will receive a floating-point number that will represent the width of the rectangle.
+            //On the second line, you will receive another floating-point number that will represent the height of the rectangle.
+            //Output
+            //Output a single line - the rectangle's area and perimeter, separated by a whitespace.
+            //Print the area and perimeter values with exactly 2-digits precision after the floating point
+
+
+
+            decimal number1 = decimal.Parse(Console.ReadLine());
+            decimal number2 = decimal.Parse(Console.ReadLine());
+
+            decimal area = number1 * number2;
+            decimal perimeter = (number1 + number2) * 2;
+
+            if (area.ToString().EndsWith("00") || perimeter.ToString().EndsWith("00")) {
+                string areaStr = area.ToString();
+                string perimeterStr = perimeter.ToString();
+                Console.WriteLine(areaStr + " " + perimeterStr);
+            }
+            else {
+                string areaStr = area.ToString() + "0";
+                string perimeterStr = perimeter.ToString() + "0";
+                Console.WriteLine(areaStr + " " + perimeterStr);
+            }
+
+            
+
+        }
+    }
+}
