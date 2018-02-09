c3a68dca71dd11da235697c7f444afa9115c77fc
Stoyan <slownessbg@gmail.com>
2018-02-09 16:45:44 +0200

    Adding finished Prime check and working on PointInCircle

---
+++ ConsoleApp1/PointInACircle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointInACircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+
+            double number = (x - 1) * (x - 1) + (y - 1) * (y - 1);
+
+            if ((x - 1) * (x - 1) + (y - 1) * (y - 1) <= 2 * 2)
+            {
+                Console.WriteLine("true {0}",number);
+            }
+            else
+            {
+                Console.WriteLine("false {0}",number);  
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/PrimeCheck/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads an integer N (which will always be less than 100 or equal) and uses an expression to check if
+            //given N is prime (i.e. it is divisible without remainder only to itself and 1).
+            //Note: You should check if the number is positive
+            //Input
+            //On the only input line you will receive the number N.
+            //Output
+            //Output true if the number is prime and false otherwise.
+
+
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input != 1 && input > 0)
+            {
+                for (int i = 1; i < input; i++)
+                {
+                    if (input % i == 0)
+                    {
+                        Console.WriteLine(true);
+                    }
+                    else
+                    {
+                        if (i == input)
+                        {
+                            Console.WriteLine(false);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            
+
+
+        }
+    }
+}
