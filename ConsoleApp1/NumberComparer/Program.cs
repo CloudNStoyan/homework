2cfd69bb182dcca1a59c7c807cacfaf7d92131a9
Stoyan <slownessbg@gmail.com>
2018-02-10 20:52:37 +0200

    Adding few unfinished programs and one finished

---
+++ ConsoleApp1/NumberComparer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that gets two numbers from the console and prints the greater of them.
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/SumOf5Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOf5Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads 5 integer numbers from the console and prints their sum.
+
+            int number = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
+            int number5 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(number + number2 + number3 + number4 + number5);
+        }
+    }
+}
