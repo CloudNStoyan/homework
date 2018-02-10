41c06e261bde54a761ca945efe15765cf1d013b4
Stoyan <slownessbg@gmail.com>
2018-02-10 19:59:51 +0200

    Fixing some stuff adding 1 Finished program and 1 unfinished

---
+++ ConsoleApp1/Circle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a program that reads from the console the radius r of a circle and prints its perimeter and area, rounded and formatted with 2 digits after the decimal point.
+            //Input
+            //On the only line of the input you will receive the radius of the circle - r
+            //Output
+            //You should print one line only: the perimeter and the area of the circle, separated by a whitespace, and with 2 digits precision
+
+            decimal p = 3.14m;
+            decimal r = decimal.Parse(Console.ReadLine());
+
+            //area pi x r^2
+            //perimetar 2pi x r
+
+            Console.WriteLine("Perimetar :" + (p * 2) * r);
+            Console.WriteLine("Area :" + p * (r * r));
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/FormattingNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            // in process of working
+        }
+    }
+}
