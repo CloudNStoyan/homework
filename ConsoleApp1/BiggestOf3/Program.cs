711e65bc67ed431bc4fff00c08c734a67b780b12
Stoyan <slownessbg@gmail.com>
2018-02-12 15:21:43 +0200

    Adding two new programms BiggestOf3 and MultiplicationSign

---
+++ ConsoleApp1/BiggestOf3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggestOf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+            } else if (b > a && b > c)
+            {
+                Console.WriteLine(b);
+            } else if (c > b && c > a)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/MultiplicationSign/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationSign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that shows the sign (+, - or 0) of the product of three real numbers, without calculating it.
+            //Use a sequence of if operators.
+
+            double inputA = double.Parse(Console.ReadLine());
+            double inputB = double.Parse(Console.ReadLine());
+            double inputC = double.Parse(Console.ReadLine());
+
+            if ((inputA == 0) || (inputB == 0) || (inputC == 0))
+            {
+                Console.WriteLine("0");
+            } else if (inputA < 0)
+            {
+                if (inputB < 0)
+                {
+                    if (inputC < 0)
+                    {
+                        Console.WriteLine("-");
+                    }
+                    else
+                    {
+                        Console.WriteLine("+");
+                    }
+                }
+                else
+                {
+                    if (inputC < 0)
+                    {
+                        Console.WriteLine("-");
+                    }
+                    else
+                    {
+                        Console.WriteLine("+");
+                    }
+                }
+            }
+            else if (inputA > 0)
+            {
+                if (inputB < 0)
+                {
+                    if (inputC < 0)
+                    {
+                        Console.WriteLine("+");
+                    }
+                    else
+                    {
+                        Console.WriteLine("-");
+                    }
+                }
+                else
+                {
+                    if (inputC < 0)
+                    {
+                        Console.WriteLine("-");
+                    }
+                    else
+                    {
+                        Console.WriteLine("+");
+                    }
+                }
+            }
+        }
+    }
+}
