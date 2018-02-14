65ebdd7c3b8c2a6b66ffc955fc13299f61319c76
Stoyan <slownessbg@gmail.com>
2018-02-14 16:17:02 +0200

    Fixing stuff

--- ConsoleApp1/Calculate!/Program.cs
+++ ConsoleApp1/Calculate!/Program.cs
@@ -13,17 +13,7 @@ namespace Calculate_
             //Write a program that, for a given two numbers N and x, calculates the sum S = 1 + 1!/x + 2!/x2 + … + N!/xN.
             //Use only one loop. Print the result with 5 digits after the decimal point.
 
-            int n = int.Parse(Console.ReadLine());
-            double x = double.Parse(Console.ReadLine());
-            int factorial = 1;
-            double sum = 1 + 1 / x;
-            for (int counter = 2; counter <= n; counter++)
-            {
-                factorial = factorial * counter;
-                double pow = Math.Pow(x, counter);
-                sum = sum + (factorial / pow);
-            }
-            Console.WriteLine("{0:F5}", sum);
+            
         }
     }
 }
