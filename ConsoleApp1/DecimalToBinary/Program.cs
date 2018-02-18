5d915d916f1574ac7b8d8c42db91c09481de0ac2
Stoyan <slownessbg@gmail.com>
2018-02-18 13:56:29 +0200

    Adding Decimal To Binary program

---
+++ ConsoleApp1/DecimalToBinary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using loops write a program that converts an integer number to its binary representation.
+            //The input is entered as long. The output should be a variable of type string.
+            //Do not use the built-in .NET functionality.
+
+            int num = int.Parse(Console.ReadLine());
+            int quot;
+            string reminder = "";
+            while (num >= 1)
+            {
+                quot = num / 2;
+                reminder += (num % 2).ToString();
+                num = quot;
+            }
+            string bin = "";
+            for (int i = reminder.Length - 1; i >= 0; i--)
+            {
+                bin = bin + reminder[i];
+            }
+            Console.WriteLine(bin);
+        }
+    }
+}
---
+++ ConsoleApp1/SpiralMatrix/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpiralMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads from the console a positive integer number N (1 ≤ N ≤ 20) and prints a matrix holding the 
+            //numbers from 1 to N*N in the form of square spiral like in the examples below.
+
+
+            int n = int.Parse(Console.ReadLine());
+
+            int coll = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                coll = i;
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(((n - n + j) + coll) + " ");
+
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
