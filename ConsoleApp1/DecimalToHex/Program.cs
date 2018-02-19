8069736c8e37976f5a46e5982846d68b60302cc6
Stoyan <slownessbg@gmail.com>
2018-02-19 16:47:38 +0200

    Adding new program DecimalToHex

---
+++ ConsoleApp1/DecimalToHex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToHex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = long.Parse(Console.ReadLine());
+            long quot = num;
+            long reminder = num % 16;
+            string hexNumber = "";
+
+            while (quot > 0)
+            {
+                quot = num / 16;
+                reminder = num % 16;
+                if (reminder > 9)
+                {
+                    reminder = reminder + 55;
+                    hexNumber = hexNumber + (char)reminder;
+                }
+                else
+                {
+                    hexNumber = hexNumber + reminder;
+                }
+                num = quot;
+            }
+            char[] charArray = hexNumber.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(new string (charArray));
+        }   
+    }
+}
