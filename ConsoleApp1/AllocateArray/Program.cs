9d00aa7f67a2129b164db7f2fcce15ed6a903f62
Stoyan <slownessbg@gmail.com>
2018-02-20 14:27:54 +0200

    Adding new program AllocateArray

---
+++ ConsoleApp1/AllocateArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllocateArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that allocates array of N integers, initializes each element by its index multiplied by 5 and the prints the obtained array on the console.
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(array[i] = i * 5);
+            }
+        }
+    }
+}
