20b53e878ad6b44d3c49d0564691cbf03b3ab609
Stoyan <slownessbg@gmail.com>
2018-02-20 14:52:34 +0200

    Adding new program CompareCharArrays

---
+++ ConsoleApp1/CompareCharArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareCharArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that compares two char arrays lexicographically (letter by letter).
+            string oneStr = Console.ReadLine();
+            string twoStr = Console.ReadLine();
+
+            int result = string.Compare(oneStr, twoStr);
+
+            if (result == 0)
+            {
+                Console.WriteLine("=");
+            }
+
+            if (result < 0)
+            {
+                Console.WriteLine("<");
+            }
+
+            if (result > 0)
+            {
+                Console.WriteLine(">");
+            }
+        }
+    }
+}
