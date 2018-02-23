005b3b57a507bea98aa23231ac3b1c0c01d3e422
Stoyan <slownessbg@gmail.com>
2018-02-23 13:56:55 +0200

    Adding new program IndexOfLetters

---
+++ ConsoleApp1/IndexOfLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that creates an array containing all letters from the alphabet (a-z). Read a word from the console and print the index of each of its letters in the array.
+            char[] letters = new char[25];
+            int j = 0;
+            for (int i = 97; i < 122; i++)
+            {
+                letters[j] = (char) i;
+                j++;
+            }
+            string input = Console.ReadLine();
+            char[] inputArray = input.ToLower().ToCharArray();
+            foreach (char element in inputArray)
+            {
+                var index = Array.FindIndex(letters, x => x == element);
+                Console.WriteLine(index);
+            }
+        }
+    }
+}
