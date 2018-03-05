4190fe5fab9a2d166ff4a8bc37a676d3e3ce1dda
Stoyan <slownessbg@gmail.com>
2018-03-05 23:17:48 +0200

    Adding new program COmbinationsOfSet

--- ConsoleApp1/CombinationsOfSet/Program.cs
+++ ConsoleApp1/CombinationsOfSet/Program.cs
@@ -10,9 +10,36 @@ namespace CombinationsOfSet
     {
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int k = int.Parse(Console.ReadLine());
-
+            int n = 5;
+            int k = 2;
+            string[] array = new string[n*k];
+            int l = 0;
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i+k-1; j < n+k-1; j++)
+                {
+                    
+                    array[l] = "| " + i.ToString();
+                    for (int m = 0; m < k-1; m++)
+                    {
+                        if (j+m-1 > n)
+                        {
+                            array[l] += " |";
+                            break;
+                        }
+                        array[l] += " , " + (j+m);
+                        if (m == k-2)
+                        {
+                            array[l] += " |";
+                        }
+                    }
+                    l++;
+                }
+            }
+            foreach (string x in array)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
