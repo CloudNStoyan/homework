b931a78300d3a5c96139382d0a1400419c7013ea
Stoyan <slownessbg@gmail.com>
2018-03-06 19:06:58 +0200

    Adding new program VartiationsOfSet

--- ConsoleApp1/VariationsOfSet/Program.cs
+++ ConsoleApp1/VariationsOfSet/Program.cs
@@ -16,12 +16,39 @@ namespace VariationsOfSet
             int n = int.Parse(Console.ReadLine());
             Console.Write("Enter K: ");
             int k = int.Parse(Console.ReadLine());
-
-            int[] array = new int[k];
-            while (true)
+            string[] array = new string[n*n];
+            int l = 0;
+            for (int i = 1; i <= n; i++)
             {
                 
-                
+                for (int j = 1; j <= n; j++)
+                {
+                    array[l] = "| " + i + ",";
+                    for (int m = 0; m <= k-2; m++)
+                    {
+                        if (m == k - 2)
+                        {
+                            array[l] += (j + m);
+                        }
+                        else
+                        {
+                            array[l] += (j + m) + ",";
+                        }
+                        if (m + j > n)
+                        {
+                            array[l] = "| ";
+                            array[l] += "=====";
+                            break;
+                        }
+                    }
+                    array[l] += " |";
+                    l++;
+                }
+            }
+            Console.Clear();
+            foreach (string s in array)
+            {
+                Console.WriteLine(s);
             }
             
         }
