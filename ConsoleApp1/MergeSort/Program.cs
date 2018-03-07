05fb8daa3c836f0927cde632143a043a8bbb32ef
Stoyan <slownessbg@gmail.com>
2018-03-07 15:42:02 +0200

    Adding new program MergeSort

--- ConsoleApp1/MergeSort/Program.cs
+++ ConsoleApp1/MergeSort/Program.cs
@@ -12,31 +12,39 @@ namespace MergeSort
         static void Main(string[] args)
         {
             //Write a program that sorts an array of integers using the Merge sort algorithm.
+            Console.Write("Input: ");
             string numbers = Console.ReadLine();
             var array = Array.ConvertAll(numbers.Split(' '), int.Parse);
-            int split = (array.Length - 1) / 2;
-            int j = split, temp = 0;
-            bool isNotFinded = true;
-
-            while (isNotFinded)
+            List<int> list = new List<int>();
+            for (int i = 0; i < array.Length; i++)
             {
-                for (int i = 0; i < array.Length - 1; i += 2)
+                list.Add(array[i]);
+            }
+            list.Sort();
+            int temp;
+            while (true)
+            {
+                if (array.SequenceEqual(list))
+                {
+                    break;
+                }
+                for (int i = 0; i < array.Length - 1; i++)
                 {
                     if (array[i] > array[i + 1])
                     {
                         temp = array[i + 1];
                         array[i + 1] = array[i];
                         array[i] = temp;
-                    }
-                    else
+                    } else if (array.SequenceEqual(list))
                     {
-                        isNotFinded = false;
+                        break;
                     }
                 }
             }
+            Console.Write("Output: ");
             foreach (int i in array)
             {
-                Console.Write(i);
+                Console.Write(i + " ");
             }
             Console.WriteLine();
         }
