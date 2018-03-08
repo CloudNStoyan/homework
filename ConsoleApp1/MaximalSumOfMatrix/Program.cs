b398c8e070a9e61295128c342d1d7eac39b4d2eb
Stoyan <slownessbg@gmail.com>
2018-03-08 16:09:13 +0200

    C# Advance: Adding new program MaximalSumOfMatrix

---
+++ ConsoleApp1/MaximalSumOfMatrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalSumOfMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+            int sum = 0;
+            for (int coll = 0; coll < matrix.GetLength(0); coll++)
+            {
+                for (int row = 0; row < matrix.GetLength(1); row++)
+                {
+                    sum += matrix[coll,row];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
