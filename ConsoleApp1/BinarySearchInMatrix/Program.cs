8122778d279a20bc3a4e42a3c83e9394ba2c7e46
Stoyan <slownessbg@gmail.com>
2018-03-09 16:26:17 +0200

    C# Advance: Adding new program SequenceInMatrix and unfinished program BinarySearchInMatrix

---
+++ ConsoleApp1/BinarySearchInMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchInMatrix
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+            int number = int.Parse(Console.ReadLine());
+            int low = 0;
+            int high = matrix.GetLength(0) * matrix.GetLength(1);
+        }
+    }
+}
---
+++ ConsoleApp1/SequenceInMatrix/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string matrixRange;
+            int[,] matrix =
+            {
+                {4, 4, 4},
+                {3, 3, 3},
+                {3, 4, 4}
+            };
+            int[] sequence = new int[matrix.GetLength(0) * matrix.GetLength(1)];
+            for (int i = 0; i < sequence.Length - 1; i++)
+            {
+                sequence[i] = 1;
+            }
+            int count = 0;
+            int temp = matrix[0,0];
+            
+            for (int coll = 0; coll < matrix.GetLength(0); coll++)
+            {
+                for (int row = 0; row < matrix.GetLength(1); row++)
+                {
+                    Console.WriteLine("| N: {0} | C: {1} | R: {2} | ", matrix[coll, row],coll,row);
+                    if (temp == matrix[coll, row])
+                    {
+                        sequence[count]++;
+                        temp = matrix[coll, row];
+                    }
+                    else
+                    {
+                        count++;
+                        temp = matrix[coll, row];
+                    }
+                    
+                }
+            }
+            Console.WriteLine("|=======L: {0} ========|",sequence.Length);
+            if (sequence.Max() > sequence.Length)
+            {
+                Console.WriteLine("|=======M: {0} ========|", sequence.Max()-1);
+            }
+            else
+            {
+            Console.WriteLine("|=======M: {0} ========|",sequence.Max());
+            }
+        }
+    }
+}
