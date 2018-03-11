269e90c9588588ed658859728aadf8a97eadfcff
Stoyan <slownessbg@gmail.com>
2018-03-11 11:16:03 +0200

    Adding new program BinarySearchInMatrix

--- ConsoleApp1/BinarySearchInMatrix/Program.cs
+++ ConsoleApp1/BinarySearchInMatrix/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace BinarySearchInMatrix
 {
     class Program
     {
-        
+       
         static void Main(string[] args)
         {
             int[,] matrix =
@@ -18,8 +19,64 @@ namespace BinarySearchInMatrix
                 {7, 8, 9}
             };
             int number = int.Parse(Console.ReadLine());
-            int low = 0;
-            int high = matrix.GetLength(0) * matrix.GetLength(1);
+            
+            int crash = 0;
+            
+
+            int low = 0, height = matrix.Length;
+
+            while (true)
+            {
+                int middle = low + ((height - low) / 2);
+                string index = findIndex(middle);
+                int coll = int.Parse(index[0].ToString()), row = int.Parse(index[2].ToString());
+
+                int candidate = matrix[coll,row];
+
+                if (candidate == number)
+                {
+                    Console.WriteLine(matrix[coll,row]);
+                    break;
+                }
+
+                if (candidate > number)
+                {
+                    height = middle;
+                }
+
+                if (candidate < number)
+                {
+                    low = middle;
+                }
+            }
+        }
+        static string findIndex(int index)
+        {
+            string output = "";
+            int coll = 0,row = 0;
+            while (index >= 0)
+            {
+                if (index >= 3)
+                {
+                    coll++;
+                    index -= 3;
+                }
+                else
+                {
+                    row = index;
+                    if (row != 0)
+                    {
+                        row--;
+                    }
+                    if (coll != 0)
+                    {
+                        coll--;
+                    }
+                    output = coll + " " + row;
+                    break;
+                }
+            }
+            return output;
         }
     }
 }
