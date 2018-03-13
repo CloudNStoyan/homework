a651fd1be395214c90597390c420cdc61070b7eb
Stoyan <slownessbg@gmail.com>
2018-03-13 20:34:37 +0200

    Adding new program SayHello

---
+++ ConsoleApp1/SayHello/Greetings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHello
+{
+    class Greetings
+    {
+
+        public static void PrintName(string name)
+        {
+            Console.WriteLine("Hello, {0}!.",name);
+        }
+    }
+}
