421a7eb27133bff0074ba820a3605f72e9c6761e
Stoyan <slownessbg@gmail.com>
2018-03-13 23:49:16 +0200

    Adding new program ReverseNumber

---
+++ ConsoleApp1/ReverseNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    class Program
+    {
+        public static int reverse(int number)
+        {
+            string strNumber = number.ToString();
+            string temp = "";
+
+            for (int i = strNumber.Length-1; i >= 0; i--)
+            {
+                temp += strNumber[i];
+            }
+            int output = int.Parse(temp);
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(reverse(456));
+        }
+    }
+}
