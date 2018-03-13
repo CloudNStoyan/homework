a3de3710c17a0736012ef367855931d6e30974fd
Stoyan <slownessbg@gmail.com>
2018-03-13 20:44:46 +0200

    Adding program descriptions

--- ConsoleApp1/GetLargestNumber/Program.cs
+++ ConsoleApp1/GetLargestNumber/Program.cs
@@ -10,6 +10,9 @@ namespace GetLargestNumber
     {
         static void Main(string[] args)
         {
+            //Write a method GetMax() with two parameters that returns the larger of two integers.
+            //Write a program that reads 3 integers from the console and prints the largest of them using the method GetMax().
+
             int[] array = new int[3];
 
             for (int i = 0; i < array.Length; i++)
--- ConsoleApp1/SayHello/Program.cs
+++ ConsoleApp1/SayHello/Program.cs
@@ -10,6 +10,7 @@ namespace SayHello
     {
         static void Main(string[] args)
         {
+            //Write a method that asks the user for his name and prints Hello, <name>!. Write a program to test this method.
             Greetings.PrintName("John");
         }
     }
