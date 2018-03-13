8be97ea4de709c0849c91adae0f813ab1933cc35
Stoyan <slownessbg@gmail.com>
2018-03-13 21:37:29 +0200

    Adding new program AppearanceCount

---
+++ ConsoleApp1/AppearanceCount/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppearanceCount
+{
+
+    class Program
+    {
+        static int countTimes(string array,int number)
+        {
+            int result = 0;
+            char[] charArray = array.ToCharArray();
+            foreach (char c in charArray)
+            {
+                string temp2 = number.ToString();
+                if (c == temp2[0])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            //Write a method that counts how many times given number appears in a given array. Write a test program to check if the method is working correctly.
+
+            string input = Console.ReadLine();
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(countTimes(input,x));
+
+        }
+    }
+}
