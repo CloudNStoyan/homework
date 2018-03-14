4c78d3fb950693ff61556e204483f9a0f846311d
Stoyan <slownessbg@gmail.com>
2018-03-14 18:17:51 +0200

    Adding new program SubstractingPolynomials

---
+++ ConsoleApp1/SubtractingPolynomials/Polynomial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SubtractingPolynomials
+{
+    class Polynomial
+    {
+        public static string Add(int[] arr1, int[] arr2)
+        {
+            string output = "";
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                output += (arr1[i] + arr2[i]) + " ";
+            }
+            return output;
+        }
+        public static string Multiply(int[] arr1, int[] arr2)
+        {
+            string output = "";
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                output += (arr1[i] * arr2[i]) + " ";
+            }
+            return output;
+        }
+        public static string Subtract(int[] arr1, int[] arr2)
+        {
+            string output = "";
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                output += (arr1[i] - arr2[i]) + " ";
+            }
+            return output;
+        }
+    }
+}
---
+++ ConsoleApp1/SubtractingPolynomials/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubtractingPolynomials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Extend the previous program to support also subtraction and multiplication of polynomials.
+            int[] arr = { 5, 0, 1 };
+            int[] arr2 = { 7, 4, -3 };
+
+            Console.WriteLine(Polynomial.Add(arr,arr2));
+            Console.WriteLine(Polynomial.Multiply(arr,arr2));
+            Console.WriteLine(Polynomial.Subtract(arr, arr2));
+        }
+    }
+}
