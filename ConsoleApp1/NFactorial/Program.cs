37a551bd33d9f8236b5db011d9932dee0836a266
Stoyan <slownessbg@gmail.com>
2018-03-14 17:25:30 +0200

    Adding new program NFactorial

---
+++ ConsoleApp1/NFactorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFactorial
+{
+    class Program
+    {
+        public static int Factorial(int n)
+        {
+            int result = n;
+            for (int i = n-1; i > 0; i--)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            //Write a method that multiplies a number represented as an array of digits by a given integer number. Write a program to calculate N!.
+            Console.WriteLine(Factorial(5));
+        }
+    }
+}
