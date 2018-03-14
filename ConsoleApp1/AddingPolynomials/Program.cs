f8e545806fc54682b8f9f29e249aad7c351c0465
Stoyan <slownessbg@gmail.com>
2018-03-14 17:32:33 +0200

    Adding new program AddingPolynomials

---
+++ ConsoleApp1/AddingPolynomials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddingPolynomials
+{
+    class Program
+    {
+        public static string polynomials(int[] arr1, int[] arr2)
+        {
+            string output = "";
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                output += (arr1[i] + arr2[i]) + " ";
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            //Write a method that adds two polynomials. Represent them as arrays of their coefficients. Write a program that reads two polynomials and prints their sum.
+            int[] arr = {5, 0, 1};
+            int[] arr2 = {7, 4, -3};
+            Console.WriteLine(polynomials(arr,arr2));
+        }
+    }
+}
