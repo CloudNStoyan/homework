e9dbe585f86774e029b76db639c28919b1b7e6f3
Stoyan <slownessbg@gmail.com>
2018-03-15 19:17:06 +0200

    Adding new program BinaryToDecimalNumSystem

---
+++ ConsoleApp1/BinaryToDecimalNumSystem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryToDecimalNumSystem
+{
+    class Program
+    {
+        static void Convert(string chars)
+        {
+            int result = 0;
+
+            for (var i = 0; i < chars.Length; i++)
+            {
+                result *= 2;
+
+                if (chars[i] == '1')
+                {
+                    result += 1;
+                }
+            }
+            Console.WriteLine(result);
+        }
+        static void Main(string[] args)
+        {
+            //Write a program that converts a binary number N to its decimal representation.
+            string input = Console.ReadLine();
+            Convert(input);
+        }
+    }
+}
