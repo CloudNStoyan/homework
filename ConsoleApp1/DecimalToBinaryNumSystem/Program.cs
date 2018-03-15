83bef895686bf066c2eb3f55c4f16cd45f718cf8
Stoyan <slownessbg@gmail.com>
2018-03-15 19:08:10 +0200

    Adding new program  DecimalToBinaryNumSystem

---
+++ ConsoleApp1/DecimalToBinaryNumSystem/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinaryNumSystem
+{
+    class Program
+    {
+        static void Convert(decimal n)
+        {
+            string temp = "";
+            while (n > 0)
+            {
+                if (n % 2 == 0)
+                {
+                    temp += "0";
+                }
+                else
+                {
+                    temp += "1";
+                }
+                n = Math.Floor(n / 2);
+            }
+            string output = "";
+            for (int i = temp.Length - 1; i >= 0; i--)
+            {
+                output += temp[i];
+            }
+            Console.WriteLine(output);
+        }
+        static void Main(string[] args)
+        {
+            decimal n = decimal.Parse(Console.ReadLine());
+            Convert(n);
+        }
+    }
+}
