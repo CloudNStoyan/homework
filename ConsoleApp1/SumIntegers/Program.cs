c49e4494953f6319bc84c9e47669e3b1f36cdedb
Stoyan <slownessbg@gmail.com>
2018-03-16 18:02:14 +0200

    Adding description to SumIntegers

--- ConsoleApp1/SumIntegers/Program.cs
+++ ConsoleApp1/SumIntegers/Program.cs
@@ -10,6 +10,9 @@ namespace SumIntegers
     {
         static void Main(string[] args)
         {
+            //You are given a sequence of positive integer values written into a string, separated by spaces. 
+            //Write a function that reads these values from given string and calculates their sum. 
+            //Write a program that reads a string of positive integers separated by spaces and prints their sum.
             string numbers = Console.ReadLine();
             var array = Array.ConvertAll(numbers.Split(' '), int.Parse);
             Console.WriteLine(array.Sum());
