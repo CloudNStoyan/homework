58b0b855fd9dab0dd016bae7b22938e7a92c5d0a
Stoyan <slownessbg@gmail.com>
2018-03-16 18:01:11 +0200

    Adding new program SumIntegers

---
+++ ConsoleApp1/SumIntegers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+            var array = Array.ConvertAll(numbers.Split(' '), int.Parse);
+            Console.WriteLine(array.Sum());
+        }
+    }
+}
