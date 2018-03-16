9c55669c2bb520adebd725517b024bc0c9c51fdf
Stoyan <slownessbg@gmail.com>
2018-03-16 17:59:09 +0200

    Adding new program DayOfWeek

---
+++ ConsoleApp1/DayOfWeek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that prints to the console which day of the week is today. Use System.DateTime.
+            DateTime a = new DateTime(2018, 3, 14);
+            Console.WriteLine(a.DayOfWeek);
+        }
+
+    }
+}
+
