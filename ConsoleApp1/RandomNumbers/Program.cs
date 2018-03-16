36626c5e6e20c5a235254f61cfd470ff36c64ebf
Stoyan <slownessbg@gmail.com>
2018-03-16 17:51:05 +0200

    Adding new program Random Numbers

---
+++ ConsoleApp1/RandomNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that generates and prints to the console 10 random values in the range [100, 200].
+            Random rnd = new Random();
+            for (int i = 0; i < 9; i++)
+            {
+                int n = rnd.Next(100, 200);
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
