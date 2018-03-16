03c10cdf06f4852074bd3d9ad216b25e94b2bb0b
Stoyan <slownessbg@gmail.com>
2018-03-16 17:45:04 +0200

    Adding new program BinaryShort and doing some work on HexToBinary

---
+++ ConsoleApp1/BinaryShort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryShort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that shows the binary representation of given 16-bit signed integer number N (the C# type short).
+
+            short num = short.Parse(Console.ReadLine());
+            int quot;
+            string reminder = "";
+            while (num >= 1)
+            {
+                quot = num / 2;
+                reminder += (num % 2).ToString();
+                num = short.Parse(quot.ToString());
+            }
+            string bin = "";
+            for (int i = reminder.Length - 1; i >= 0; i--)
+            {
+                bin = bin + reminder[i];
+            }
+            Console.WriteLine(bin);
+        }
+    }
+}
---
+++ ConsoleApp1/HexadecimalToBinary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexadecimalToBinary
+{
+    class Program
+    {
+        static void Convert(string input)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            //Write a program to convert hexadecimal numbers to binary numbers (directly).
+            string hexNumber = Console.ReadLine();
+                
+
+        }
+    }
+}
