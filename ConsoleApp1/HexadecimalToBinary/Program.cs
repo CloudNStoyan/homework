ea8cba6e5bf0f94badc6c7f975e0c6be383f4c8e
Stoyan <slownessbg@gmail.com>
2018-03-17 13:00:52 +0200

    Adding new program HexToBinary

--- ConsoleApp1/HexadecimalToBinary/Program.cs
+++ ConsoleApp1/HexadecimalToBinary/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HexadecimalToBinary
 {
@@ -10,14 +8,38 @@ namespace HexadecimalToBinary
     {
         static void Convert(string input)
         {
-            
+            var map = new Dictionary<string, string>
+            {
+                {"0", "0000"},
+                {"1", "0001"},
+                {"2", "0010"},
+                {"3", "0011"},
+                {"4", "0100"},
+                {"5", "0101"},
+                {"6", "0110"},
+                {"7", "0111"},
+                {"8", "1000"},
+                {"9", "1001"},
+                {"A", "1010"},
+                {"B", "1011"},
+                {"C", "1100"},
+                {"D", "1101"},
+                {"E", "1110"},
+                {"F", "1111"},
+            };
+
+            string output = string.Join("", input.Select(c => map[c.ToString()])).TrimStart('0');
+
+            Console.WriteLine(output);
         }
         static void Main(string[] args)
         {
             //Write a program to convert hexadecimal numbers to binary numbers (directly).
-            string hexNumber = Console.ReadLine();
-                
-
+            while (true)
+            {
+                string hexNumber = Console.ReadLine();
+                Convert(hexNumber);
+            }
         }
     }
 }
