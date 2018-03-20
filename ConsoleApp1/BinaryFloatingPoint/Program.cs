59c2f7374e9f9858726e61f1db5fc7392928d164
Stoyan <slownessbg@gmail.com>
2018-03-20 18:19:16 +0200

    Adding new program BinaryFloatingPoint

---
+++ ConsoleApp1/BinaryFloatingPoint/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryFloatingPoint
+{
+    class Program
+    {
+        static string FloatNumberToBinary(float number)
+        {
+            int dec = BitConverter.ToInt32(BitConverter.GetBytes(number), 0);
+
+            return Convert.ToString(dec, 2);
+        }
+
+        static string GetExponent(string binary)
+        {
+            return binary.Substring(0, 8);
+        }
+
+        static string GetMantissa(string binary)
+        {
+            return binary.Substring(8);
+        }
+
+        static void Main()
+        {
+            //Write a program that shows the internal binary representation of given 32-bit signed floating-point number in IEEE 754 format (the C# type float).
+            float number = float.Parse(Console.ReadLine());
+            int sign = number < 0 ? 1 : 0;
+            number = Math.Abs(number);
+            string binary = FloatNumberToBinary(number);
+            if ((int) number == 0 || (int) number == 1)
+            {
+                binary = "0" + binary;
+            }
+
+            bool hasSign = sign == 1;
+            if (hasSign)
+            {
+                Console.WriteLine("Sign: (minus)");
+            }
+            else
+            {
+                Console.WriteLine("Sign: (plus)");
+            }
+            Console.WriteLine("Exponent: {0}", GetExponent(binary));
+            Console.WriteLine("Mantissa: {0}\n", GetMantissa(binary));
+        }
+    }
+}
