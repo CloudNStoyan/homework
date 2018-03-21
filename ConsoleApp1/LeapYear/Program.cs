669b8bc7d5e915fb453a0f447f54fe48ed8d6c22
Stoyan <slownessbg@gmail.com>
2018-03-21 15:35:22 +0200

    Adding new program LeapYear

---
+++ ConsoleApp1/LeapYear/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static bool isLeapYear(int year)
+        {
+            if (DateTime.DaysInMonth(year, 2) == 29)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int year = int.Parse(Console.ReadLine());
+                if (isLeapYear(year))
+                {
+                    Console.WriteLine("Leap");
+                }
+                else
+                {
+                    Console.WriteLine("Common");
+                }
+            }
+        }
+    }
+}
