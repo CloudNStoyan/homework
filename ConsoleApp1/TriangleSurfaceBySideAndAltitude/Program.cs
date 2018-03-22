1cebc109c21138838c6fbf4362c5a381cd41e669
Stoyan <slownessbg@gmail.com>
2018-03-22 13:10:18 +0200

    Adding new program TriangleSurfaceBySlideAndAltitude

---
+++ ConsoleApp1/TriangleSurfaceBySideAndAltitude/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSurfaceBySideAndAltitude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write program that calculates the surface of a triangle by given a side and an altitude to it.
+            while (true)
+            {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine((a * b) / 2);
+            }
+        }
+    }
+}
