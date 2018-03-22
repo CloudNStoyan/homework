0920fee7997e7bb69113ca46216978900f11f244
Stoyan <slownessbg@gmail.com>
2018-03-22 13:50:11 +0200

    Adding new program TriangleSurfaceByTwoSidesAndAnAngle

---
+++ ConsoleApp1/TriangleSurfaceByTwoSidesAndAnAngle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSurfaceByTwoSidesAndAnAngle
+{
+    class Program
+    {
+
+        static double toRadians(double degs)
+        {
+            return Math.PI * degs / 180;
+        }
+        static void Main(string[] args)
+        {
+            //Write program that calculates the surface of a triangle by given two sides and an angle between them.
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double sinC = Math.Sin(toRadians(double.Parse(Console.ReadLine())));
+
+            double area = a * b * sinC / 2;
+            Console.WriteLine("{0:F2}",area);
+        }
+    }
+}
