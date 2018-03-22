d99c9b019a3c19f404f9b00ca8c6547a5afee713
Stoyan <slownessbg@gmail.com>
2018-03-22 13:17:32 +0200

    Adding new program TriangleSurfaceByThreeSides

---
+++ ConsoleApp1/TriangleSurfaceByThreeSides/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSurfaceByThreeSides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write program that calculates the surface of a triangle by given its three sides.
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            double p = (a + b + c) / 2;
+
+            double area = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            Console.WriteLine("{0:F2}",area);
+        }
+    }
+}
