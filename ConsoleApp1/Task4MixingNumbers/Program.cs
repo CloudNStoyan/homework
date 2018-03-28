c8e32f2255e6172a4f864d143eed12e39a134f87
Stoyan <slownessbg@gmail.com>
2018-03-28 19:07:24 +0300

    Adding new program Task4MixingNumbers

---
+++ ConsoleApp1/Task4MixingNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4MixingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = (Console.ReadLine() + "").Split(' ').ToArray();
+            string firstLine = "";
+            string secondLine = "";
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i + 1 >= numbers.Length)
+                {
+                    break;
+                }
+                firstLine += int.Parse(numbers[i][1].ToString()) * int.Parse(numbers[i + 1][0].ToString()) + " ";
+                secondLine += Math.Abs(int.Parse(numbers[i]) - int.Parse(numbers[i + 1])) + " ";
+            }
+
+            Console.WriteLine(firstLine);
+            Console.WriteLine(secondLine);
+        }
+    }
+}
