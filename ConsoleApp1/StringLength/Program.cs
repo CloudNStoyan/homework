4f0b112b9876c78fb9088017c942f70b4b9685d2
Stoyan <slownessbg@gmail.com>
2018-03-31 21:22:25 +0300

    Adding new program StringLength

---
+++ ConsoleApp1/StringLength/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads from the console a string of maximum 20 characters.
+            //If the length of the string is less than 20, the rest of the characters should be filled with *.
+
+            string input = Console.ReadLine() + "";
+
+            if (input.Length < 20)
+            {
+                for (int i = input.Length; i < 20; i++)
+                {
+                    input += "*";
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
