9258928919b2abef61a855b565d971c6cb422629
Stoyan <slownessbg@gmail.com>
2018-04-01 21:54:26 +0300

    Adding new program ExtractSentences

---
+++ ConsoleApp1/ExtractSentences/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractSentences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that extracts from a given text all sentences containing given word.
+
+            string word = " " + Console.ReadLine() + " ";
+            string text = Console.ReadLine();
+
+            string[] array = text.Split('.').ToArray();
+
+            foreach (string s in array)
+            {
+                if (s.Contains(word))
+                {
+                    Console.WriteLine(s + ".");
+                }
+            }
+        }
+    }
+}
