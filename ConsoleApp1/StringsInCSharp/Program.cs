354dcea63c09899eabdd9d7514b0afc7a912615d
Stoyan <slownessbg@gmail.com>
2018-04-01 21:13:46 +0300

    Adding new program StringInCSharp

---
+++ ConsoleApp1/StringsInCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Describe the strings in C#. What is typical for the string data type? Describe the most important methods of the String class.
+
+            //String is array of characters
+            //String is immutable(read only)
+            //Everytime when you edit string you make another one
+            //Most important methods:
+            //.ToUpperCase();
+            //.toLowerCase();
+            //.Substring();
+            //.Length
+            //.ToCharArray();
+            //.Trim();
+            //.TrimStart();
+            //.TrimEnd();
+            //.Compare();
+            //.Equals();
+            //.IndexOf();
+            //.LastIndexOf();
+            //.Replace();
+            //.Split();
+        }
+    }
+}
