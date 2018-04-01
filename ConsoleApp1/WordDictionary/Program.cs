a28162d348a4b2868da40bca900054e57d924fee
Stoyan <slownessbg@gmail.com>
2018-04-01 22:48:15 +0300

    Adding new program WordDictionary

---
+++ ConsoleApp1/WordDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A dictionary is stored as a sequence of text lines containing words and their explanations. Write a program that enters a word 
+            //and translates it by using the dictionary.
+
+            string word = Console.ReadLine();
+
+            var dic = new Dictionary<string,string>
+            {
+                {".NET", "platform for applications from Microsoft"},
+                {"CLR", "managed execution environment for .NET"},
+                {"namespace", "hierarchical organization of classes"}
+            };
+
+            if (dic.ContainsKey(word))
+            {
+                Console.WriteLine(dic[word]);
+            }
+            else
+            {
+                Console.WriteLine("There is no description about {0}",word);
+            }
+        }
+    }
+}
