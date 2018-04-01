88a07ca55cf017bbc09e8e7a200b9d8bed96a3b3
Stoyan <slownessbg@gmail.com>
2018-04-01 21:04:45 +0300

    Adding new program SubstringInText

---
+++ ConsoleApp1/SubstringInText/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that finds how many times a sub-string is contained in a given text (perform case insensitive search).
+
+            string word = Console.ReadLine() + "".ToLower();
+            string text = Console.ReadLine() + "".ToLower();
+            int index = 0, count = 0;
+            while (true)
+            {
+                index = text.IndexOf(word, index + word.Length);
+                if (index < 0)
+                {
+                    break;
+                }
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
