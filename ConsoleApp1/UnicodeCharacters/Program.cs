2cdb2461cb11270247f6b893e1119f170e7eae5e
Stoyan <slownessbg@gmail.com>
2018-04-01 22:29:35 +0300

    Adding new program UnicodeCharacters

---
+++ ConsoleApp1/UnicodeCharacters/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that converts a string to a sequence of C# Unicode character literals.
+            string word = Console.ReadLine();
+            StringBuilder result = new StringBuilder();
+
+            foreach (char c in word)
+            {
+                int decimalRepresentation = c;
+                result.Append(ConvertToUnicode(decimalRepresentation));
+            }
+
+            Console.WriteLine(result.ToString());
+        }   
+
+        static string ConvertToUnicode(decimal n)
+        {
+            string prefix = @"\u00";
+            decimal quot = n;
+            decimal remainder = n % 16;
+            string hexNumber = "";
+
+            while (quot > 0)
+            {
+                quot = Math.Floor(n / 16);
+                remainder = n % 16;
+                if (remainder > 9)
+                {
+                    remainder = remainder + 55;
+                    hexNumber += (char)remainder;
+                }
+                else
+                {
+                    hexNumber +=  remainder;
+                }
+                n = quot;
+            }
+            char[] charArray = hexNumber.ToCharArray();
+            Array.Reverse(charArray);
+            string result = new string(charArray);
+            return prefix + result;
+        }
+    }
+}
