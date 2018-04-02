44aa87d08c4129d3610d7aa67fdee93b9d436796
Stoyan <slownessbg@gmail.com>
2018-04-02 15:35:49 +0300

    Adding new program Palindromes

---
+++ ConsoleApp1/Palindromes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that extracts from a given text all palindromes, e.g. ABBA, lamal, exe.
+            string[] text = Console.ReadLine().Split(' ');
+            string[] palindroms = Array.FindAll(text, isPalindrom);
+            foreach (string palindrom in palindroms)
+            {
+                Console.WriteLine(palindrom);
+            }
+        }
+
+        static bool isPalindrom(string word)
+        {
+            List<char> array = new List<char>();
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                array.Add(word[i]);
+            }
+
+            string editedWord = new string(array.ToArray());
+            if (word == editedWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
