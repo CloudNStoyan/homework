f3c9106199353688b2afa6b2a9650c1d57f2a7b1
Stoyan <slownessbg@gmail.com>
2018-04-02 12:04:35 +0300

    Adding new program FormatNumber

---
+++ ConsoleApp1/FormatNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormatNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads a number and prints it as a decimal number, hexadecimal number, percentage and in scientific 
+            //notation. Format the output aligned right in 15 symbols.
+
+            int number = int.Parse(Console.ReadLine() + "");
+
+
+            Console.WriteLine("Decimal: {0,15:D}",number);
+            Console.WriteLine("Hexadecimal: {0,15:X}",number);
+            Console.WriteLine("Percentage: {0,15:p}",number);
+            Console.WriteLine("Scientific Notation: {0,15:E}",number);
+        }
+    }
+}
