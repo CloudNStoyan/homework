a35dcdbdcb5be816b389a2e6108f4ebb47595a22
Stoyan <slownessbg@gmail.com>
2018-04-02 13:15:23 +0300

    Adding new program ReverseSentence

---
+++ ConsoleApp1/ReverseSentence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseSentence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reverses the words in a given sentence.
+
+            string[] sentences = Console.ReadLine().Split(' ');
+            StringBuilder output = new StringBuilder();
+
+            for (int i = sentences.Length - 1; i >= 0; i--)
+            {
+                output.Append(sentences[i] + " ");
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+    }
+}
