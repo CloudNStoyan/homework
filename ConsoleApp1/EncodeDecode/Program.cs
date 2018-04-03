debbd3f8539951f3603b51affcf4a145d2d4d60b
Stoyan <slownessbg@gmail.com>
2018-04-03 20:33:38 +0300

    Adding new program EncodeDecode

---
+++ ConsoleApp1/EncodeDecode/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodeDecode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string key = Console.ReadLine();
+            Console.WriteLine("Encryption: {0}", Encrypt(word, key));
+            Console.WriteLine("Decryption: {0}", Decrypt(Encrypt(word, key),key));
+        }
+
+        static string Encrypt(string word, string key)
+        {
+            StringBuilder cipher = new StringBuilder();
+            for (int i = 0; i < word.Length; i++)
+                cipher.Append((char)(word[i] ^ key[i % key.Length]));
+            return cipher.ToString();
+        }
+
+        static string Decrypt(string word, string key)
+        {
+            return Encrypt(word, key);
+        }
+    }
+}
