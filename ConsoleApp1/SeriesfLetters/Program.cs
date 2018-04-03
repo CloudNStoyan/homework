9977453f9be32c666ab43df439459a92d788d81c
Stoyan <slownessbg@gmail.com>
2018-04-03 15:36:38 +0300

    Adding new program SeriesOfLetter

---
+++ ConsoleApp1/SeriesfLetters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SeriesfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Console.WriteLine(Regex.Replace(text, @"(\w)\1+", "$1"));
+        }
+    }
+}
