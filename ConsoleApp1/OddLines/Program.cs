35b331971e79a451a9ae425371adea1f48bdddbf
Stoyan <slownessbg@gmail.com>
2018-04-05 13:06:28 +0300

    Adding new program OddLines

---
+++ ConsoleApp1/OddLines/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that reads a text file and prints on the console its odd lines.
+
+            string path = @"text.txt";
+            var encoding = Encoding.UTF8;
+            if (File.Exists(path))
+            {
+                using (var stream = new StreamReader(path, encoding))
+                {
+                    try
+                    {
+                        List<string> oddLines = new List<string>();
+                        string line = stream.ReadLine();
+                        int lineNumber = 1;
+                        while (line != null)
+                        {
+                            if (lineNumber % 2 != 0)
+                            {
+                                oddLines.Add(line);
+                            }
+
+                            lineNumber++;
+                            line = stream.ReadLine();
+                        }
+
+                        foreach (string oddLine in oddLines)
+                        {
+                            Console.WriteLine(oddLine);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    finally
+                    {
+                        stream.Close();
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"File {path} doesn't exist!");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/OddLines/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OddLines")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OddLines")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aca6b0f1-b041-4c1d-92fe-8a6ae4b61b0a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
