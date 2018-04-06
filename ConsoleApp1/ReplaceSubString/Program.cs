859dd72f2233345206754a038a84b089c164ef21
Stoyan <slownessbg@gmail.com>
2018-04-06 13:15:52 +0300

    Adding new program ReplaceSubString

---
+++ ConsoleApp1/ReplaceSubString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceSubString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program that replaces all occurrences of the sub-string start with the sub-string finish in a text file. Ensure it will 
+            //work with large files (e.g. 100 MB).
+
+            string path = "text.txt";
+            string text = File.ReadAllText(path);
+            text = text.Replace("start", "finish");
+            File.WriteAllText(path,text);
+        }
+    }
+}
