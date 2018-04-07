90482fb79b17619ade58ced7da000dee2fdc8494
Stoyan <slownessbg@gmail.com>
2018-04-07 15:38:42 +0300

    Adding new program ComparingStrings

---
+++ ConsoleApp1/ComparingStrings/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // compares two strings lexicographically, optionally ignoring the case
+
+            var result = Compare("wow", "ad", false);
+            Console.WriteLine(result[0] + " " + result[1]);
+        }
+
+        static string[] Compare(string left, string right, bool ignoreCase)
+        {
+            string[] result = new string[2];
+
+            int length = Math.Min(left.Length, right.Length);
+
+            for (int i = 0; i < length; i++)
+            {
+                if (ignoreCase)
+                {
+                    left = left.ToLower();
+                    right = right.ToLower();
+                }
+                if (left[i] < right[i])
+                {
+                    result[0] = left;
+                    result[1] = right;
+                    return result;
+                } else if (left[i] > right[i])
+                {
+                    result[0] = right;
+                    result[1] = left;
+                    return result;
+                }
+            }
+
+            if (left.Length > right.Length)
+            {
+                result[0] = left;
+                result[1] = right;
+            }
+            else
+            {
+                result[0] = right;
+                result[1] = left;
+            }
+            return result;
+        }
+    }
+}
