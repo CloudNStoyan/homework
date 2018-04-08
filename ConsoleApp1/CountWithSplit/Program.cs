5c388d834872c6bd6d58d9227c4b4eef97acc27f
Stoyan <slownessbg@gmail.com>
2018-04-08 14:05:25 +0300

    Adding new program CountWithSplit

---
+++ ConsoleApp1/CountWithSplit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountWithSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // count occurrences of a string within another string via split
+            // NOTE: doesnt work for strings like "aa"
+
+            var text = "baconaaaaaaa stuff bacon stuff stuff baconbacon yes";
+            var splitText = text.Split(new[] { "yes" },StringSplitOptions.None);
+            Console.WriteLine(splitText.Length - 1);
+        }
+    }
+}
