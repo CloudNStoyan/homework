41eee7f6209eaaaa20edeece8a4517b9246633ec
Stoyan <slownessbg@gmail.com>
2018-04-09 17:21:08 +0300

    Adding new program SanitizeText

---
+++ ConsoleApp1/SanitizeText/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanitizeText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sanitize user input against XSS
+            // < &#60;
+            // > &#62;
+
+            var badName = "<script>alert('hello gosho');</script>";
+
+            string[] lines = badName.Split(new[] { Environment.NewLine },
+                StringSplitOptions.None);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains('<'))
+                {
+                    lines[i] = lines[i].Replace("<", "&#60");
+                }
+
+                if (lines[i].Contains('>'))
+                {
+                    lines[i] = lines[i].Replace(">", "&#62");
+                }
+            }
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+        }
+    }
+}
