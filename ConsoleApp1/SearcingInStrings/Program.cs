e54958fea8a57c1a252edf9e84b59f07c600f907
Stoyan <slownessbg@gmail.com>
2018-04-09 17:47:51 +0300

    Adding new program SaerchingInString

---
+++ ConsoleApp1/SearcingInStrings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearcingInStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find all occurrences of a word within a text and return the indices of the occurrences
+
+            var text = "gg b gg pesho ivan gg";
+
+            var indexs = Occurences(text, "g");
+
+            foreach (int i in indexs)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static List<int> Occurences(string str,string word)
+        {
+            var indexs = new List<int>();
+
+
+            int index = str.IndexOf(word);
+
+            while (index != -1)
+            {
+                indexs.Add(index);
+                index = str.IndexOf(word, index + word.Length);
+            }
+            return indexs;
+        }
+    }
+}
