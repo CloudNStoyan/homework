734b2e99a52bd4cd9ac376fff783fe8d8ed2845f
Stoyan <slownessbg@gmail.com>
2018-04-12 15:13:58 +0300

    Adding new program BinaryDigitsCount

---
+++ ConsoleApp1/BinaryDigitsCount/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryDigitsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int binary = int.Parse(Console.ReadLine() + "");
+            int length = int.Parse(Console.ReadLine() + "");
+
+            int[] results = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                uint number = uint.Parse(Console.ReadLine() + "");
+                int count = 0;
+                while (number >= 1)
+                {
+                    if (number % 2 == binary)
+                    {
+                        count++;
+                    }
+                    number /= 2;
+                }
+
+                results[i] = count;
+            }
+
+            foreach (int result in results)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
