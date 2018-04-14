b09c8f974cc552526750d666239af553e5c2889d
Stoyan <slownessbg@gmail.com>
2018-04-14 14:32:05 +0300

    Adding new program OddNumber

---
+++ ConsoleApp1/OddNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine() + "");
+            long[] arr = new long[n];
+
+            var dic = new Dictionary<long,int>();
+
+            long oddOne = 0;
+
+            for (long i = 0; i < n; i++)
+            {
+                arr[i] = long.Parse(Console.ReadLine() + "");
+                if (dic.ContainsKey(arr[i]))
+                {
+                    dic[arr[i]]++;
+                }
+                else
+                {
+                    dic.Add(arr[i],1);
+                }
+            }
+
+            foreach (var item in dic)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    oddOne = item.Key;
+                }
+            }
+
+            Console.WriteLine(oddOne);
+        }
+    }
+}
