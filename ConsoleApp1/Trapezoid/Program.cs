af58ee8cecad2f60713ac2adf9f7f62db5523032
Stoyan <slownessbg@gmail.com>
2018-04-14 14:32:25 +0300

    Adding new program Trapezoid

---
+++ ConsoleApp1/Trapezoid/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trapezoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine() + "");
+
+            int widthTop = n;
+            int widthBot = 2 * n;
+            int height = n + 1;
+
+            int minus = 2;
+
+            for (int i = 0; i < height; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < widthBot; j++)
+                    {
+                        if (j >= n - 1)
+                        {
+                            Console.Write('*');
+                        }
+                        else
+                        {
+                            Console.Write('.');
+                        }
+                    }
+                }
+
+                if (i != 0 && i + 1 != height)
+                {
+                    for (int j = 0; j < widthBot - 1; j++)
+                    {
+                        if (j == (n - minus))
+                        {
+                            minus++;
+                            Console.Write('*');
+                        }
+                        else
+                        {
+                            Console.Write('.');
+                        }
+                    }
+
+                    Console.Write('*');
+                }
+
+                if (i + 1 == height)
+                {
+                    for (int j = 0; j < widthBot; j++)
+                    {
+                        Console.Write('*');
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
