fd9c963c5897e0347091595c20f02b1449c2738b
Stoyan <slownessbg@gmail.com>
2018-04-16 20:08:12 +0300

    Adding new program Employees unfinished and Sudoku unfinished

---
+++ ConsoleApp1/Employees/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var job = new Dictionary<string, int>();
+            List<string> names = new List<string>();
+            List<string> ranks = new List<string>();
+            List<string> output = new List<string>();
+
+            int jobsNumber = int.Parse(Console.ReadLine() + "");
+            for (int i = 0; i < jobsNumber; i++)
+            {
+                string[] line = Console.ReadLine().Split('-');
+                string key = line[0].Trim();
+                int value = int.Parse(line[1]);
+
+                job.Add(key,value);
+            }
+
+            int namesNumber = int.Parse(Console.ReadLine() + "");
+            for (int i = 0; i < namesNumber; i++)
+            {
+                string[] line = Console.ReadLine().Split('-');
+                string name = line[0].Trim();
+                string rank = line[1].Trim();
+
+                names.Add(name);
+                ranks.Add(rank);
+            }
+
+            var orderedRanks = ranks;
+            orderedRanks.Sort();
+
+            Console.WriteLine("------------------------------");
+            int lower = 100000;
+            int oldLower = 0;
+            var ids = new Dictionary<int,int>();
+
+
+            while (output.Count != names.Count)
+            {
+                bool throughAll = false;
+                int index = 0;
+                for (int i = 0; i < names.Count; i++)
+                {
+                    if (job[ranks[i]] < lower && !ids.ContainsKey(job[ranks[i]]))
+                    {
+                        lower = job[ranks[i]];
+                        index = i;
+                    }
+
+                    
+                }
+
+                Console.WriteLine(oldLower + " > " + lower);
+                if (!ids.ContainsKey(lower))
+                {
+                    ids.Add(lower, 0);
+                }
+                output.Add(names[index]);
+                oldLower = lower;
+            }
+
+
+            foreach (string s in output)
+            {
+                Console.WriteLine(s);
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Sudoku/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class Program
+    {
+        static int[,] unSolvedSudoku = new int[9, 9];
+        static int[,] solvedSudoku;
+        static int[] currentPos = new []{0,0};
+
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            for (int i = 0; i < 9; i++)
+            {
+                lines.Add(Console.ReadLine());
+            }
+
+            // 0 = 1,0 2,0 3,0
+            // 1 = 0,0 0,2 0,3
+
+            for (int i = 0; i < unSolvedSudoku.GetLength(0); i++)
+            {
+                for (int j = 0; j < unSolvedSudoku.GetLength(1); j++)
+                {
+                    if (char.IsDigit(lines[i][j]))
+                    {
+                        unSolvedSudoku[i,j] = int.Parse(lines[i][j].ToString());
+                    }
+                    else
+                    {
+                        unSolvedSudoku[i, j] = 0;
+                    }
+                }
+            }
+            solvedSudoku = unSolvedSudoku;
+
+            /*int[] result = new int[2];
+            result[0] = 1;
+            result[1] = 3;
+            Console.WriteLine(isValid(1,result));*/
+            while (isNotSolved())
+            {
+
+            }
+
+            Print(unSolvedSudoku);
+        }
+
+        static void Print(int[,] sudoku)
+        {
+            for (int i = 0; i < sudoku.GetLength(0); i++)
+            {
+                for (int j = 0; j < sudoku.GetLength(1); j++)
+                {
+                    Console.Write("|" + sudoku[i,j]);
+                }
+
+                Console.WriteLine("|");
+            }
+        }
+
+        static bool isNotSolved()
+        {
+            for (int i = 0; i < solvedSudoku.GetLength(0); i++)
+            {
+                for (int j = 0; j < solvedSudoku.GetLength(1); j++)
+                {
+                    if (solvedSudoku[i,j] == 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        static bool isValid(int number, int[] possition)
+        {
+            if (!Up(number, possition) || !Down(number, possition) || !Right(number, possition) || !Left(number, possition))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool Up(int number,int[] possition)
+        {
+            for (int i = possition[0] + 1; i < solvedSudoku.GetLength(0); i++)
+            {
+                if (solvedSudoku[i, possition[1]] == number)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool Down(int number, int[] possition)
+        {
+            for (int i = possition[0] - 1; i >= 0; i--)
+            {
+                if (solvedSudoku[i, possition[1]] == number)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool Right(int number, int[] possition)
+        {
+            for (int i = possition[1] + 1; i < solvedSudoku.GetLength(1); i++)
+            {
+                if (solvedSudoku[possition[0], i] == number)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool Left(int number, int[] possition)
+        {
+            for (int i = possition[1] - 1; i >= 0; i--)
+            {
+                if (solvedSudoku[possition[0], i] == number)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
