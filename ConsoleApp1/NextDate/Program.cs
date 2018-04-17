82efa03a69518894da1671e8a9aded89f9e4bf56
Stoyan <slownessbg@gmail.com>
2018-04-17 18:05:08 +0300

    Adding new program NextDate

---
+++ ConsoleApp1/NextDate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextDate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine() + "");
+            int month = int.Parse(Console.ReadLine() + "");
+            int year = int.Parse(Console.ReadLine() + "");
+
+
+            var date = new DateTime(year,month,day);
+            date = date.AddDays(1);
+            Console.WriteLine(date.Day + "." + date.Month + "." + date.Year);
+        }
+    }
+}
