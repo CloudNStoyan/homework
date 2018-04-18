37e32fbed1fa7d527606c4fa875b77c344e6c066
Stoyan <slownessbg@gmail.com>
2018-04-18 15:45:45 +0300

    Adding new program Tribonacci

---
+++ ConsoleApp1/Tribonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tribonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            for (int i = 0; i < 3; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine() + ""));
+            }
+            int howManyLeft = int.Parse(Console.ReadLine() + "");
+
+            for (int i = numbers.Count; numbers.Count != howManyLeft; i++)
+            {
+                numbers.Add(numbers[i - 1] + numbers[i - 2] + numbers[i - 3]);
+            }
+
+            Console.WriteLine(numbers[numbers.Count - 1]);
+        }
+    }
+}
