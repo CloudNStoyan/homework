8a5fddaf68bfc3db58de35822a9f41a81f18f26a
Stoyan <slownessbg@gmail.com>
2018-04-18 15:46:08 +0300

    Adding new program FirTree

---
+++ ConsoleApp1/FirTree/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine() + "");
+            int width = height - 2;
+            int star = 1;
+
+            for (int i = 0; i < height; i++)
+            {
+                if (i + 1 >= height)
+                {
+                    for (int j = 0; j < height - 2; j++)
+                    {
+                        Console.Write('.');
+                    }
+
+                    Console.Write('*');
+
+                    for (int j = 0; j < height - 2; j++)
+                    {
+                        Console.Write('.');   
+                    }
+
+                    Console.WriteLine();
+                }
+                else
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        Console.Write('.');
+                    }
+
+                    for (int j = 0; j < star; j++)
+                    {
+                        Console.Write('*');
+                    }
+
+                    for (int j = 0; j < width; j++)
+                    {
+                        Console.Write('.');
+                    }
+
+                    star += 2;
+                    width -= 1;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
