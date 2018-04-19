dde7a3ad8445ff39e0d6d8502216daf77e4dc100
Stoyan <slownessbg@gmail.com>
2018-04-19 16:40:14 +0300

    Adding new program SandGlass

---
+++ ConsoleApp1/SandGlass/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandGlass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine() + "");
+            int width = height;
+            int center = Convert.ToInt32(Math.Floor(Convert.ToDouble(height / 2)));
+            int inputWidth = width;
+
+            for (int i = 0; i < height; i++)
+            {
+                if (i <= center)
+                {
+                    if (i != 0)
+                    {
+                        width -= 2;
+                    }
+                    int dot = (inputWidth - width) / 2;
+                    for (int j = 0; j < dot; j++)
+                    {
+                        Console.Write('.');
+                    }
+                    for (int j = 0; j < width; j++)
+                    {
+                        Console.Write('*');
+                    }
+                    for (int j = 0; j < dot; j++)
+                    {
+                        Console.Write('.');
+                    }
+                    Console.WriteLine();
+                }
+                else
+                {
+                    width += 2;
+                    int dot = (inputWidth - width) / 2;
+                    for (int j = 0; j < dot; j++)
+                    {
+                        Console.Write('.');
+                    }
+                    for (int j = 0; j < width; j++)
+                    {
+                        Console.Write('*');
+                    }
+                    for (int j = 0; j < dot; j++)
+                    {
+                        Console.Write('.');
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }   
+    }
+}
