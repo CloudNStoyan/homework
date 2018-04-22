c9158184dc74cd845a041357bdcf13928698e060
Stoyan <slownessbg@gmail.com>
2018-04-22 19:07:58 +0300

    Adding new program DancingBits and doing work on SubsetSums

---
+++ ConsoleApp1/DancingBits/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DancingBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine() + "");
+            int n = int.Parse(Console.ReadLine() + "");
+            string bits = "";
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine() + "");
+                bits += Convert.ToString(number, 2);
+            }
+
+            int allSequence = (k > 1) ? 0 : 1;
+            int currSequence = 1;
+            for (int i = 1; i < bits.Length; i++)
+            {
+                if (currSequence == k)
+                {
+                    if (i + 1 < bits.Length)
+                    {
+                        if (bits[i + 1] == bits[i])
+                        {
+                            currSequence = 1;
+                        }
+                        else
+                        {
+                            allSequence++;
+                            currSequence = 1;
+                        }
+                    }
+                    else
+                    {
+                        allSequence++;
+                    }
+                }
+                if (bits[i - 1] == bits[i])
+                {
+                    currSequence++;
+                }
+            }
+
+            Console.WriteLine(allSequence);
+        }
+    }
+}
---
+++ ConsoleApp1/SubsetSums/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubsetSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = int.Parse(Console.ReadLine() + "");
+            int lines = int.Parse(Console.ReadLine() + "");
+            int[] arr = new int[lines];
+            for (int i = 0; i < lines; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine() + "");
+            }
+
+            int[,] matrix = new int[lines,sum + 1];
+
+            for (int i = 0; i < sum + 1; i++)
+            {
+                Console.Write("| " + i +" |");
+            }
+
+            Console.WriteLine();
+            Print(matrix);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == 0)
+                    { 
+
+                    }
+                }
+            }
+
+
+        }
+
+        static void Print(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("| " + matrix[i,j] + " |");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
