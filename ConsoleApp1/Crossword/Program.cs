988cb2e885ae7b2a229540ed34efdeef5101eb21
Stoyan <slownessbg@gmail.com>
2018-05-11 15:26:37 +0300

    Finishing with Crossword

--- ConsoleApp1/Crossword/Program.cs
+++ ConsoleApp1/Crossword/Program.cs
@@ -16,10 +16,10 @@ namespace Crossword
 
             for (int i = 0; i < n * 2; i++)
             {
-                _lines[i] = Console.ReadLine();
+                _lines.Add( Console.ReadLine());
             }
 
-            var crosswords = new List<string>();
+            var crosswords = GetAllCollWords(n);
 
             for (int i = 0; i < _lines.Count; i++)
             {
@@ -28,8 +28,65 @@ namespace Crossword
                     crosswords.Add(_lines[i]);
                 }
             }
+
+            Console.Clear();
+
+            if (crosswords.Count > 0)
+            {
+                foreach (var str in crosswords)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO SOLUTION!");
+            }
         }
 
+        static List<string> GetAllCollWords(int n)
+        {
+            int length = n;
+
+            var collwords = new List<string>();
+
+            int increase = 0;
+            while (length < (n - 1 ) * 2) 
+            {
+                for (int charIndex = 0; charIndex < n; charIndex++)
+                {
+                    var word = new StringBuilder();
+                    for (int lineIndex = 0 + increase; lineIndex < length; lineIndex++)
+                    {
+                        word.Append(_lines[lineIndex][charIndex]);
+                    }
+
+                    if (ThereIsWord(word.ToString()))
+                    {
+
+                        collwords.Add(word.ToString());
+                    }
+                }
+
+                length++;
+                increase++;
+            }
+
+            return collwords;
+        }
+
+        static bool ThereIsWord(string word)
+        {
+            foreach (string line in _lines)
+            {
+                if (line == word)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         static bool FindExpectThis(string line, int index)
         {
