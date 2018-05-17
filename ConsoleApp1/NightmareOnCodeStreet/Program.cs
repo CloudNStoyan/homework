c64d1a32ee67afd6c24a9eb499ac203a14f41a3f
Stoyan <slownessbg@gmail.com>
2018-05-17 18:38:29 +0300

    Adding new program NightmareOnCodeStreet

---
+++ ConsoleApp1/NightmareOnCodeStreet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightmareOnCodeStreet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine() + "";
+            int length = 0;
+            int sum = 0;
+
+            for (int i = 1; i < input.Length; i+= 2)
+            {
+                length++;
+                sum += int.Parse(input[i] + "");
+            }
+
+            Console.WriteLine($"{length} {sum}");
+        }
+    }
+}
