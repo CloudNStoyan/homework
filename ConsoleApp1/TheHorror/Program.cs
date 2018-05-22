9e187d3b58e57428c3f03b06da1d8c8bea87237a
Stoyan <slownessbg@gmail.com>
2018-05-22 15:21:15 +0300

    Adding new program TheHorror

---
+++ ConsoleApp1/TheHorror/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHorror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int sum = 0, howMany = 0;
+
+            for (int i = 0; i < input.Length; i+= 2)
+            {
+                sum += int.Parse(input[i] + "");
+                howMany++;
+            }
+
+            Console.WriteLine($"{howMany} {sum}");
+        }
+    }
+}
