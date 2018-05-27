ce0b6801b78f7a2cf8b3511059fb4ebe2fe55629
Stoyan <slownessbg@gmail.com>
2018-05-27 21:07:11 +0300

    Adding new program SumOfEvenDivisors

---
+++ ConsoleApp1/SumOfEvenDivisors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfEvenDivisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int from = int.Parse(Console.ReadLine() + "");
+            int to = int.Parse(Console.ReadLine() + "");
+
+            var evenDivisors = new List<int>();
+
+            for (int i = from; i < to + 1; i++)
+            {
+                for (int j = 1; j < i + 1; j++)
+                {
+                    if (i % j == 0 && j % 2 == 0)
+                    {
+                        evenDivisors.Add(j);
+                    }
+                }
+            }
+
+            Console.WriteLine(evenDivisors.Sum());
+        }
+    }
+}
