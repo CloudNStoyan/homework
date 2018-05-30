5f9f6d1df80c8d6710acfcaf746f698293fce120
Stoyan <slownessbg@gmail.com>
2018-05-30 22:15:40 +0300

    Adding new program Money

---
+++ ConsoleApp1/Money/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Money
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
+
+            int n = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+
+            double each = p / 400;
+
+
+            Console.WriteLine("{0:F3}",(n * s) * each);
+        }
+    }
+}
