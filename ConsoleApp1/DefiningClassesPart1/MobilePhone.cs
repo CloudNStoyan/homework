3d5b0a99ff676e4232aef68a83a062cbe8be89ea
Stoyan <slownessbg@gmail.com>
2018-06-05 22:45:33 +0300

    Starting the project doing work with fields

---
+++ ConsoleApp1/DefiningClassesPart1/MobilePhone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClassesPart1
+{
+    class MobilePhone
+    {
+        /*
+         *model, manufacturer, price, owner, battery characteristics (model, hours idle and hours talk) and display characteristics (size and number of colors).
+         *
+         */
+
+        public string model;
+        public string manufacturer;
+        public double price;
+        public string owner;
+        //Battery
+        public string modelOfBattery;
+        public int hoursIdle;
+        public int hoursTalk;
+        public double size;
+        public int numberOfCalors;
+
+
+    }
+}
---
+++ ConsoleApp1/DefiningClassesPart1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefiningClassesPart1
+{
+    class Program
+    {
+        private static MobilePhone myPhone = new MobilePhone();
+        static void Main(string[] args)
+        {
+            myPhone.owner = "Stoyan";
+            myPhone.hoursIdle = 3;
+            myPhone.hoursTalk = 2;
+            myPhone.manufacturer = "Stoyan CO";
+            myPhone.modelOfBattery = "PS22287";
+            myPhone.numberOfCalors = 2;
+            myPhone.size = 166;
+            myPhone.model = "Galaxy S5";
+            myPhone.price = 929.99;
+
+            Console.WriteLine(myPhone.owner);
+        }
+    }
+}
