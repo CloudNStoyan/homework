45c576d28f7f48a66406ff8daa7769b11cc0acdf
Stoyan <slownessbg@gmail.com>
2018-06-10 18:07:15 +0300

    Adding new PathStorage class which save and load Paths from txt files

--- ConsoleApp1/DefiningClassesPart2/Startup.cs
+++ ConsoleApp1/DefiningClassesPart2/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,10 +48,78 @@ namespace DefiningClassesPart2
 
     public class Path
     {
-        private Point3D[] Points3D;
+        private static Point3D[] PathPoints3D;
         public Path(params Point3D[] points)
         {
-            Points3D = points;
+            PathPoints3D = points;
+        }
+
+        public Point3D Get(int index)
+        {
+            return PathPoints3D[index];
+        }
+
+        public override string ToString()
+        {
+            return ConvertToString(PathPoints3D);
+        }
+
+        private string ConvertToString(Point3D[] points)
+        {
+            var build = new StringBuilder();
+            for (int i = 0; i < points.Length; i++)
+            {
+                build.AppendLine(points[i].ToString());
+            }
+
+            return build.ToString().Trim();
+        }
+    }
+
+    public static class PathStorage
+    {
+        public static string DicPath = @"C:\Users\Stoyan\Documents\temp\";
+        public static void Save(Path path)
+        {
+            if (!File.Exists($"{DicPath}Path-{Date()}.txt"))
+            {
+                File.WriteAllText($"{DicPath}Path-{Date()}.txt",path.ToString());
+                Console.WriteLine("Saved!");
+            }
+            else
+            {
+                Console.WriteLine("File already exist!");
+            }
+        }
+
+        public static Path Load(string name)
+        {
+            if (File.Exists($"{DicPath}{name}.txt"))
+            {
+                var points = File.ReadAllLines($"{DicPath}{name}.txt");
+
+                var Points3D = new Point3D[points.Length];
+
+                for (int i = 0; i < Points3D.Length; i++)
+                {
+                    int x = int.Parse(points[i].Split(',')[0]);
+                    int y = int.Parse(points[i].Split(',')[1]);
+                    int z = int.Parse(points[i].Split(',')[2]);
+                    Points3D[i] = new Point3D(x,y,z);
+                }
+
+                return new Path(Points3D);
+            }
+            else
+            {
+                Console.WriteLine($"There is no Path with the name {name} in {DicPath}!");
+                return new Path(new Point3D(0,0,0));
+            }
+        }
+
+        public static string Date()
+        {
+            return $"{DateTime.Now.Month.ToString().PadLeft(2, '0')}-{DateTime.Now.Day.ToString().PadLeft(2, '0')}-{DateTime.Now.Hour.ToString().PadLeft(2, '0')}-{DateTime.Now.Minute.ToString().PadLeft(2,'0')}-{DateTime.Now.Second.ToString().PadLeft(2, '0')}";
         }
     }
 
@@ -62,6 +131,11 @@ namespace DefiningClassesPart2
             Point3D mySecondPoint = new Point3D(-1,-2,-1);
             Point3D myThirdPoint = new Point3D(0,1,0);
 
+
+            Path myPath = new Path(myFirstPont,mySecondPoint,myThirdPoint);
+
+            var myLoadedPath = PathStorage.Load("Path-06-10-17-48-56");
+            Console.WriteLine(myLoadedPath);
         }
     }
 }
