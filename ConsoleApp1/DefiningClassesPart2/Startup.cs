ef76da1b1396948558376a9743a03f0501907cb2
Stoyan <slownessbg@gmail.com>
2018-06-10 16:34:40 +0300

    Adding new program DeffiningClassesPart2 with struct with overrided ToString()

---
+++ ConsoleApp1/DefiningClassesPart2/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClassesPart2
+{
+    public struct Point3D
+    {
+        private static int X;
+        private static int Y;
+        private static int Z;
+
+        public Point3D(int x,int y,int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override string ToString()
+        {
+            return $"{X},{Y},{Z}";
+        }
+    }
+
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            Point3D myPoint3D = new Point3D(1,2,3);
+        }
+    }
+}
