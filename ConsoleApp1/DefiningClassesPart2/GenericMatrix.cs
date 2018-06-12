b1131dd967e89ba7cd7bfe20691f6ce0cee2636b
Stoyan <slownessbg@gmail.com>
2018-06-12 22:10:33 +0300

    Adding implemented operation + and - and operator true

--- ConsoleApp1/DefiningClassesPart2/GenericMatrix.cs
+++ ConsoleApp1/DefiningClassesPart2/GenericMatrix.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,12 +13,33 @@ namespace DefiningClassesPart2
         private T[,] elements;
         private int InitialCapacityRow = 20;
         private int InitialCapacityCol = 20;
+
+        public int CurrentCapacityRow
+        {
+            get { return this.elements.GetLength(0); }
+        }
+
+        public int CurrentCapacityCol
+        {
+            get { return this.elements.GetLength(1); }
+        }
+
         public GenericMatrix(int inputRow,int inputCol)
         {
             int row = inputRow > InitialCapacityRow ? inputRow : InitialCapacityRow;
             int col = inputCol > InitialCapacityCol ? inputCol : InitialCapacityCol;
 
             elements = new T[row,col];
+        }
+
+        public GenericMatrix(T[,] matrix)
+        {
+            elements = matrix;
+        }
+
+        public T[,] ToMatrix()
+        {
+            return elements;
         }
 
         public GenericMatrix()
@@ -29,6 +51,26 @@ namespace DefiningClassesPart2
         {
             get { return this.elements[row,col]; }
             set { this.elements[row,col] = value; }
+        }
+
+        public static GenericMatrix<T> operator +(GenericMatrix<T> firstGenericMatrix,GenericMatrix<T> secondGenericMatrix)
+        {
+            var firstMatrix = firstGenericMatrix.ToMatrix();
+            var secondMatrix = secondGenericMatrix.ToMatrix();
+
+            var resultMatrix = new T[firstMatrix.GetLength(0),firstMatrix.GetLength(1)];
+
+            for (int i = 0; i < firstMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < firstMatrix.GetLength(1); j++)
+                {
+                    //Some more work
+                }
+            }
+
+            var resultGenericMatrix = new GenericMatrix<T>(resultMatrix);
+
+            return resultGenericMatrix;
         }
     }
 }
