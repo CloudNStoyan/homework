5776335308f51af9d84406a1931a236ef131fe86
Stoyan <slownessbg@gmail.com>
2018-06-13 18:28:01 +0300

    Adding new program Extension which features: Stringuilber.Substring()

---
+++ ConsoleApp1/Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class Extensions
+    {
+        public static StringBuilder Substring(this StringBuilder builder,int index,int length)
+        {
+            string text = builder.ToString();
+            text = text.Substring(index, length);
+
+            return new StringBuilder(text);
+        }
+    }
+}
---
+++ ConsoleApp1/Extensions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var build = new StringBuilder();
+
+            build.Append("az sum dobur");
+            
+        }
+    }
+}
