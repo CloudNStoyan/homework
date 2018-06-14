9205698539e66dd225976abf68413beafc4aadb9
Stoyan <slownessbg@gmail.com>
2018-06-14 20:35:40 +0300

    Adding AgeRange method

--- ConsoleApp1/Extensions/Program.cs
+++ ConsoleApp1/Extensions/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +11,43 @@ namespace Extensions
 {
     class Program
     {
+        class Student
+        {
+            private string FirstName;
+            private string SecondName;
+            private int StudentAge;
+
+            public Student(string fName, string sName, int age)
+            {
+                FirstName = fName;
+                SecondName = sName;
+                StudentAge = age;
+            }
+
+            public string Name 
+            {
+                get { return FirstName; }
+            }
+
+            public string FamilyName
+            {
+                get { return SecondName; }
+            }
+
+            public int Age
+            {
+                get { return StudentAge; }
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
+            var StudentOne = new Student("Stoyan","Kolev",10);
+            var StudentTwo = new Student("Ahmed","Ahmedov",20);
+
+            var arrOfStudents = new Student[] {StudentOne, StudentTwo};
 
         }
 
@@ -18,5 +55,11 @@ namespace Extensions
         {
             return arr.Where(name => string.Compare(name.Split(' ')[0], name.Split(' ')[1]) > 0).OrderBy(name => name).ToArray();
         }
+
+        static string[] AgeRange(Student[] arr)
+        {
+            return arr.Where(student => student.Age >= 18 && student.Age <= 24).Select(student => $"{student.Name} {student.FamilyName}").ToArray();
+        }
+        
     }
 }
