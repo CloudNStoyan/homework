f322aadd5994e49e5b6085b6bf11d14351689c23
Stoyan <slownessbg@gmail.com>
2018-06-16 13:28:57 +0300

    Adding new method ExtractMarks

--- ConsoleApp1/Extensions/Program.cs
+++ ConsoleApp1/Extensions/Program.cs
@@ -12,47 +12,30 @@ namespace Extensions
 {
     class Program
     {
-        class SomeStudent
+
+        class Group
         {
-            private string FirstName;
-            private string LastName;
-            private int StudentAge;
+            public int GroupNumber { get; private set; }
+            public string DepartmentName { get; private set; }
 
-            public SomeStudent(string fName, string sName, int age)
+            public Group(int gNum, string departName)
             {
-                FirstName = fName;
-                LastName = sName;
-                StudentAge = age;
+                GroupNumber = gNum;
+                DepartmentName = departName;
             }
-
-            public string Name
-            {
-                get { return FirstName; }
-            }
-
-            public string FamilyName
-            {
-                get { return LastName; }
-            }
-
-            public int Age
-            {
-                get { return StudentAge; }
-            }
-
         }
 
         class Student
         {
-            private string FirstName;
-            private string LastName;
-            private int FN;
-            private string Tel;
-            private string Email;
-            private string Marks;
-            private int GroupNumber;
+            public string FirstName { get; private set; }
+            public string LastName { get; private set; }
+            public int FN { get; private set; }
+            public string Tel { get; private set; }
+            public string Email { get; private set; }
+            public List<int> Marks { get; private set; }
+            public int GroupNumber { get; private set; }
 
-            public Student(string fName, string lName, int fn, string tel, string email, string marks, int groupNumber)
+            public Student(string fName, string lName, int fn, string tel, string email, List<int> marks, int groupNumber)
             {
                 FirstName = fName;
                 LastName = lName;
@@ -62,27 +45,6 @@ namespace Extensions
                 Marks = marks;
                 GroupNumber = groupNumber;
             }
-
-            public int GroupNum
-            {
-                get { return GroupNumber; }
-
-            }
-
-            public string Name
-            {
-                get { return $"{FirstName} {LastName}"; }
-            }
-
-            public string Mail
-            {
-                get { return Email; }
-            }
-
-            public string PhoneNumber
-            {
-                get { return Tel; }
-            }
         }
 
         delegate void TimerPredicate(string text);
@@ -90,20 +52,37 @@ namespace Extensions
 
         static void Main(string[] args)
         {
-            Student Stoyan = new Student("Stoyan","Rex",29,"+35924141","slowness@abv.bg","Stuff",4);
-            Student Kaloqn = new Student("Kaloqn", "Erwicht", 31, "+35944141", "sasdasdasda@gmail.bg", "Stuff", 2);
-            Student Pesho = new Student("Pesho", "Grung", 4432, "+35984141", "nww@cmd.eu", "Stuff", 2);
-            Student Jordan = new Student("Jordan", "Esnicht", 229, "0214141", "erw@abv.bg", "Stuff", 3);
-            Student Tosho = new Student("Tosho", "Bulev", 25, "+35914141", "sle@abv.bg", "Stuff", 1);
+            Student Stoyan = new Student("Stoyan","Rex",294306,"+35924141","slowness@abv.bg",new List<int>(){2,4,5},4);
+            Student Kaloqn = new Student("Kaloqn", "Erwicht", 294306, "+35944141", "sasdasdasda@gmail.bg", new List<int>() { 6, 4, 5 }, 2);
+            Student Tosho = new Student("Tosho", "Bulev", 294316, "+35914141", "sle@abv.bg", new List<int>() { 2, 3, 3,6 }, 1);
+            Student Pesho = new Student("Pesho", "Grung", 294352, "+35984141", "nww@cmd.eu", new List<int>() { 2, 6, 5 }, 2);
+            Student Jordan = new Student("Jordan", "Esnicht", 293305, "0214141", "erw@abv.bg", new List<int>() { 2, 5, 5 }, 3);
 
             var MyStudents = new Student[] {Stoyan, Kaloqn, Jordan, Pesho, Tosho};
 
-            var arr2 = ExtractByPhone(MyStudents, "2");
+            var arr = ExtractMarks(MyStudents);
 
-            var allStrs = new string[] {"Edin", "Dvaaaaaaaa", "O", "Cheteri"};
-            Console.WriteLine(GetLargest(allStrs));
+
         }
 
+        static dynamic[] ExtractByMarks(Student[] arr)
+        {
+            arr = arr.Where(student => student.Marks.Contains(6)).ToArray();
+            
+            dynamic[] myStudents = new dynamic[arr.Length];
+            for (int i = 0; i < myStudents.Length; i++)
+            {
+                myStudents[i] = new {FullName = $"{arr[i].FirstName} {arr[i].LastName}", Marks = arr[i].Marks};
+            }
+
+            return myStudents;
+
+        }
+
+        static List<List<int>> ExtractMarks(Student[] arr)
+        {
+            return arr.Where(student => student.FN.ToString().EndsWith("06")).Select(student => student.Marks).ToList();
+        }
 
 
         static string GetLargest(string[] arr)
@@ -134,33 +113,23 @@ namespace Extensions
 
         static List<Student> ExtractByPhone(Student[] arr, string cityCode)
         {
-            return arr.Where(student => student.PhoneNumber.StartsWith($"+359{cityCode}") || student.PhoneNumber.StartsWith($"0{cityCode}"))
+            return arr.Where(student => student.Tel.StartsWith($"+359{cityCode}") || student.Tel.StartsWith($"0{cityCode}"))
                 .ToList();
         }
 
         static List<Student> ExtractByMail(Student[] arr,string mail)
         {
-            return arr.Where(student => student.Mail.EndsWith(mail)).ToList();
+            return arr.Where(student => student.Email.EndsWith(mail)).ToList();
         }
 
         static List<Student> FromGroup(Student[] arr, int gn)
         {
-            return arr.Where(student => student.GroupNum == gn).ToList();
+            return arr.Where(student => student.GroupNumber == gn).ToList();
         }
 
         static string[] FirstBeforeLast(string[] arr)
         {
             return arr.Where(name => string.Compare(name.Split(' ')[0], name.Split(' ')[1]) > 0).OrderBy(name => name).ToArray();
-        }
-
-        static string[] AgeRange(SomeStudent[] arr)
-        {
-            return arr.Where(student => student.Age >= 18 && student.Age <= 24).Select(student => $"{student.Name} {student.FamilyName}").ToArray();
-        }
-
-        static SomeStudent[] OrderSomeStudents(SomeStudent[] arr)
-        {
-            return arr.OrderByDescending(student => student.Name).ThenByDescending(student => student.FamilyName).ToArray();
         }
 
         static int[] DivisibleBy7And3(int[] numbers)
