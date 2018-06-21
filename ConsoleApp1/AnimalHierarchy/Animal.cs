1ba222874853fe8078f0549474cffffe4e180d62
Stoyan <slownessbg@gmail.com>
2018-06-21 18:46:22 +0300

    Creating the basics

---
+++ ConsoleApp1/AnimalHierarchy/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalHierarchy
+{
+    class Animal
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Sex { get; private set; }
+
+        public Animal(string name, int age, string sex)
+        {
+            Name = name;
+            Age = age;
+            Sex = sex;
+        }
+    }
+}
