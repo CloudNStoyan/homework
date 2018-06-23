28de3b99478e350bc19e66efd28810e09698af31
Stoyan <slownessbg@gmail.com>
2018-06-23 20:03:06 +0300

    Adding the bases of new program Shapes

---
+++ ConsoleApp1/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+   public abstract class Shape
+    {
+        public static double CalculateSurface()
+        {
+            return 0;
+        }
+    }
+}
