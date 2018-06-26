4902f496b25d4b5a0e3ce40cbea0fe44adfaac0b
Stoyan <slownessbg@gmail.com>
2018-06-26 17:00:30 +0300

    Adding new class RangeException with constructor

---
+++ ConsoleApp1/RangeExceptions/InvalidRangeException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RangeExceptions
+{
+    public class InvalidRangeException<T>
+    {
+        private string errorMessage;
+        private string errorCondition;
+
+        public InvalidRangeException(string message,string condition)
+        {
+            errorMessage = message;
+            errorCondition = condition;
+        }
+    }
+}
