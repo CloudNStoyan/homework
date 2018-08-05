3c290649f77bcfd6d44b6976b927d43121884373
Stoyan <slownessbg@gmail.com>
2018-08-05 20:06:57 +0300

    Adding new homework GetClassPropertiesAndPrintThem

---
+++ ConsoleApp1/GetClassPropertiesAndPrintThem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetClassPropertiesAndPrintThem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < typeof(SomeClass).GetProperties().Length; i++)
+            {
+                Console.WriteLine(typeof(SomeClass).GetProperties()[i]);
+            }
+        }
+    }
+
+    class SomeClass
+    {
+        public string SomeProp1 { get; set; }
+        public int SomeProp2 { get; set; }
+
+        public SomeClass()
+        {
+
+        }
+    }
+}
