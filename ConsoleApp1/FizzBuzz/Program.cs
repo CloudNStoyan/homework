fa0362810ea4c7f4ace4c7634ca85d4b0698088b
Stoyan <slownessbg@gmail.com>
2019-02-20 22:42:17 +0200

    Adding new program FizzBuzz

---
+++ ConsoleApp1/FizzBuzz/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<int, string>()
+            {
+                {3, "Fizz"},
+                {5, "Buzz"}
+            };
+
+
+            for (int i = 1; i <= 100; i++)
+            {
+                var keys = dictionary.Keys;
+                string text = "";
+
+                foreach (int key in keys)
+                {
+                    if (i % key == 0)
+                    {
+                        text += dictionary[key];
+                    }
+                }
+
+                if (string.IsNullOrEmpty(text))
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Console.WriteLine(text);
+                }
+            }
+        }
+    }
+}
