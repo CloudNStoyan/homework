cafe654e2a428e18dd342cfd202340b130fedfb2
Stoyan <slownessbg@gmail.com>
2019-02-28 21:07:11 +0200

    Stuff with async and await

---
+++ ConsoleApp1/TryingAsyncAndAwait/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryingAsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpRequest("https://www.google.com/");
+            Console.WriteLine("Dwarf Dwarf");
+            Console.ReadLine();
+        }
+
+        private static async void HttpRequest(string url)
+        {
+            Console.WriteLine("Started fetching...");
+            var client = new HttpClient();
+            string response = await client.GetStringAsync(url);
+            Console.WriteLine("Finished fetching.");
+
+        }
+    }
+}
