caa23189bdc8e740257068b62d5599e592936819
Stoyan <slownessbg@gmail.com>
2019-03-08 20:15:25 +0200

    Adding new program ConsoleInputWithoutBlockingThread

---
+++ ConsoleApp1/ConsoleInputWithoutBlockingThread/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleInputWithoutBlockingThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Im doing my stuff");
+                Thread.Sleep(1000);
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(false);
+                    Console.WriteLine(Environment.NewLine + "You pressed: " + key.Key);
+                }
+            }
+        }
+    }
+}
